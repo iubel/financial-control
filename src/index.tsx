470a5a042434445e122b6934bb40490d06942415
Matheus Iubel <matheus-iubel@hotmail.com>
2022-10-30 17:50:29 -0300

    Finish form settings

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createServer } from 'miragejs';
+import { createServer, Model } from 'miragejs';
 import { App } from './App';
 
 createServer({
+  models: {
+    transaction: Model,
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Website creation',
+          type: 'deposit',
+          category: 'Work',
+          amount: 5000,
+          createdAt: new Date('2022-10-30 09:00:00'),
+        },
+        {
+          id: 2,
+          title: 'Notebook',
+          type: 'withdraw',
+          category: 'Costs work',
+          amount: 300,
+          createdAt: new Date('2022-10-25 09:00:00'),
+        }
+      ]
+    })
+  },
+
   routes() {
     this.namespace = 'api';
     this.get('/transactions', () => {
-      return [
-        {
-          id: 1,
-          title: 'transation 1',
-          amount: 400,
-          type: 'deposit',
-          category: 'food',
-          createdAt: new Date()
-        }
-      ]
+      return this.schema.all('transaction');
+    })
+
+    this.post('/transactions', (schema, request) => {
+      const data = JSON.parse(request.requestBody)
+
+      return schema.create('transaction', data);
     })
   }
 })
