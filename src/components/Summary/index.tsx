b0b183b3fa50e691c9720f5890a01c551b9ce94c
Matheus Iubel <matheus-iubel@hotmail.com>
2022-10-30 19:28:13 -0300

    Finished calculating resume

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -9,6 +9,31 @@ import { Container } from "./styles";
 export function Sumary() {
     const { transactions } = useContext(TransactionsContext);
 
+    //Example to use in totalDeposits
+    // const totalDeposits = transactions.reduce((acc, transaction) => {
+    //     if (transaction.type === 'deposit') {
+    //         return acc + transaction.amount;
+    //     }
+
+    //     return acc;
+    // }, 0);
+
+    const summary = transactions.reduce((acc, transaction) => {
+        if (transaction.type === 'deposit') {
+            acc.deposits += transaction.amount;
+            acc.total += transaction.amount;
+        } else {
+            acc.withdraws += transaction.amount;
+            acc.total -= transaction.amount;
+        }
+
+        return acc;
+    }, {
+        deposits: 0,
+        withdraws: 0,
+        total: 0,
+    })
+
     return (
         <Container>
             <div>
@@ -16,7 +41,12 @@ export function Sumary() {
                     <p>Incomes</p>
                     <img src={incomeImg} alt="Incomes" />
                 </header>
-                <strong>R$1000,00</strong>
+                <strong>
+                    {new Intl.NumberFormat('pt-BR', {
+                        style: 'currency',
+                        currency: 'BRL',
+                    }).format(summary.deposits)}
+                </strong>
             </div>
 
             <div>
@@ -24,7 +54,13 @@ export function Sumary() {
                     <p>Outcomes</p>
                     <img src={outcomeImg} alt="Outcomes" />
                 </header>
-                <strong>- R$500,00</strong>
+                <strong>
+                    -
+                    {new Intl.NumberFormat('pt-BR', {
+                        style: 'currency',
+                        currency: 'BRL',
+                    }).format(summary.withdraws)}
+                </strong>
             </div>
 
             <div className="highlight-background">
@@ -32,7 +68,12 @@ export function Sumary() {
                     <p>Total</p>
                     <img src={totalImg} alt="Total" />
                 </header>
-                <strong>R$500,00</strong>
+                <strong>
+                    {new Intl.NumberFormat('pt-BR', {
+                        style: 'currency',
+                        currency: 'BRL',
+                    }).format(summary.total)}
+                </strong>
             </div>
         </Container>
     )
