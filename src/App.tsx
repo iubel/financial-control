886edab6ccfb86059863a7f5a2f16bccd97b4005
Matheus Iubel <matheus-iubel@hotmail.com>
2022-10-30 18:32:57 -0300

    Create transactions context

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { useState } from 'react';
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
 import { NewTransactionModal } from "./components/NewTransactionModal";
+import { TransactionsProvider } from "./TransactionsContext";
 
 Modal.setAppElement('#root');
 
@@ -19,7 +20,7 @@ export function App() {
   }
 
   return (
-    <>
+    <TransactionsProvider>
       <Header
         onOpenNewTransactionModal={handelOpenNewTransactionModal}
       />
@@ -32,6 +33,6 @@ export function App() {
       />
 
       <GlobalStyle />
-    </>
+    </TransactionsProvider>
   );
 }
